82c7064186cebc8f58f7f96f5bdfa0f4577ac87b
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-08-18 23:38:17 -0700

    add more stopwatch-like solution for keypress

--- solutions/keypress/base.js
+++ solutions/keypress/base.js
@@ -2,20 +2,28 @@ console.log("Sanity Check: JS is working!");
 
 var startTime; // undefined
 var endTime; // undefined
-var total; // undefined
+var total = 0;
 
 $(document).ready(function(){
-
   $(window).on("keypress", function handleKeypress(event){
 
     if ( startTime ) {
       endTime = Date.now();
       total = (endTime - startTime) / 1000;
-      $("#total-time").text( total + " seconds" )
+      displayTotal();
     } else {
       startTime = Date.now();
+      // now that start has been clicked,
+      // use a JavaScript timing function to call
+      // displayTotal every second!
+      window.setInterval(displayTotal, 500);
     }
 
   })
 
 })
+
+function displayTotal(){
+  total = (Date.now() - startTime) / 1000;
+  $("#total-time").text( total + " seconds" );
+}
---
+++ solutions/keypress/stopwatch.js
@@ -0,0 +1,47 @@
+console.log("Sanity Check: JS is working!");
+
+// track the most recent "start" time
+var startTime; // undefined
+
+// track the most recent "end" time
+var endTime; // undefined
+
+// a boolean to keep track of whether the timer is on or off
+var counting = false;
+
+// variable used for continuous time update while counting
+var repeatDisplay;
+
+// stores total time counted so far
+var total = 0;
+
+$(document).ready(function(){
+
+  $(window).on("keypress", function handleKeypress(event){
+    if (!counting){
+      // we weren't counting,
+      // so this click should start the timer...
+      counting = true;
+      // ... and record the start time...
+      startTime = Date.now();
+      // ... and start updating the display every 100 milliseconds
+      repeatDisplay = setInterval(showCurrentTotal, 100);
+    } else {
+      // we were already counting,
+      // so this click should stop the timer...
+      counting = false;
+      // ... and stop updating the display automatically...
+      clearInterval(repeatDisplay);
+      // ... and update and display the total
+      endTime = (Date.now() - startTime)/1000;
+      total = total + endTime;
+      $("#total-time").text( total + " seconds" );
+    }
+  })
+
+})
+
+function showCurrentTotal(){
+  elapsedTime = (Date.now() - startTime)/1000;
+  $("#total-time").text( (total + elapsedTime) + " seconds" );
+}
